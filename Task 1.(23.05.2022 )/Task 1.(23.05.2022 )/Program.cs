588b032433cfced932c1884fd6ddd5ef1ff2eb93
Khagani89 <khaganichh@code.edu.az>
2022-05-24 12:20:54 +0400

    Task 1

---
+++ Task 1.(23.05.2022 )/Task 1.(23.05.2022 )/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task_1._23._05._2022__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1 , 2, 3, 4 , 5 , 6 };
+            var n = 3;
+         
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == n)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
+
